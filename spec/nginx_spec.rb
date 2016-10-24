b5c945a58fc892425a6dabeda7dc170c30e59c75
FGtatsuro <fujiistorage@gmail.com>
2016-10-24 19:35:42 +0900

    Add specs to confirm whether Nginx configs are set.

--- spec/nginx_spec.rb
+++ spec/nginx_spec.rb
@@ -3,3 +3,16 @@ require "spec_helper_#{ENV['SPEC_TARGET_BACKEND']}"
 describe command('which nginx') do
   its(:exit_status) { should eq 0 }
 end
+
+[
+  "#{ENV['NGINX_ROOT_CONFIG_REMOTE']}",
+  "#{ENV['NGINX_EXTRA_CONFIGS_REMOTE_DIR']}/site1.json",
+  "#{ENV['NGINX_EXTRA_CONFIGS_REMOTE_DIR']}/site2.json"
+].each do |f|
+  describe file(f) do
+    it { should be_file }
+    it { should be_readable }
+    it { should be_owned_by ENV['NGINX_CONFIG_OWNER'] }
+    it { should be_grouped_into ENV['NGINX_CONFIG_GROUP'] }
+  end
+end
